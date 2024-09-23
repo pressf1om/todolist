b2b2846cb3af1f0df8cfdd65a26630618dda7707
pressf1om <wwwsasha.www@yandex.ru>
2024-09-23 21:35:08 +0300

    + enter

--- todolist/forum/forms.py
+++ todolist/forum/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from .models import ForumPost, Comment
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = ForumPost
         fields = ['title', 'content']
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
